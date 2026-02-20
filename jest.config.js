8ecb864180a4d8d34aa2f1b62107258688815a93
Arijit <arijit.saha@zysk.tech>
2026-02-20 17:03:53 +0530

    Task 10 - Music Discovery - Medium - Feature

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ module.exports = {
 				"**/__tests__/task3/**/*.test.{ts,tsx}",
 				"**/__tests__/task8/**/*.test.{ts,tsx}",
 				"**/__tests__/task9/**/*.test.{ts,tsx}",
+				"**/__tests__/task10/**/*.test.{ts,tsx}",
 			],
 			testPathIgnorePatterns: ["/node_modules/", "search\\.service\\.test\\.ts$", "tracks\\.service\\.test\\.ts$"],
 			moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
